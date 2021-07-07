0f3023de10bee620e9b5f0989aa1d7dd92e9ffb3
oscarolsson94 <oscar.olsson94@hotmail.com>
2021-07-07 14:21:06 +0200

    adding item name and price to cartItem

--- src/components/Cart/CartItem/CartItem.jsx
+++ src/components/Cart/CartItem/CartItem.jsx
@@ -8,6 +8,10 @@ const CartItem = ({item}) => {
     return (
         <Card>
             <CardMedia image={item.media.source} alt={item.name} className={classes.media} />
+            <CardContent className={classes.cardContent} >
+                <Typography variant="h4">{item.name}</Typography>
+                <Typography variant="h5">{item.line_total.formatted_with_symbol}</Typography>
+            </CardContent>
         </Card>
     )
 }

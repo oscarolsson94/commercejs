d3f6d9e4dee065b6e4222b32c944e49f0db36fa2
oscarolsson94 <oscar.olsson94@hotmail.com>
2021-07-07 09:42:01 +0200

    creating navbar component

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography } from '@material-ui/core';
+import { ShoppingCart } from '@material-ui/icons';
+import { classExpression } from '@babel/types';
+
+const Navbar = () => {
+    return (
+        <>
+            <AppBar position="fixed" className={classExpression.appBar} color="inherit" >
+                <Toolbar>
+                    <Typography variant="h6" className={classes.title} color="inherit" >
+                        <img src={} alt="Commerce.js" height="25px" className={classes.image} />
+                        Company name
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+        </>
+    )
+}
+
+export default Navbar;

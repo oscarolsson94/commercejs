6179d355acd32a0e8afc55c0cdb8bf7d45c5da95
oscarolsson94 <oscar.olsson94@hotmail.com>
2021-07-07 10:28:02 +0200

    adding styles and adding space under navbar

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Grid } from '@material-ui/core';
+
 import Product from './Product/Product';
+import useStyles from './styles';
 
 const products = [
     {id: 1, name: 'Shoes', description: 'Running shoes.', price: '$5', image: ''},
@@ -8,7 +10,9 @@ const products = [
 ];
 
 const Products = () => {
-    return <main>
+    const classes = useStyles();
+    return <main className={classes.content} >
+        <div className={classes.toolbar} />
         <Grid container justify="center" spacing={4} >
             {products.map((product) => (
                 <Grid item key={product.id} xs={12} sm={6} md= {4} lg={3}>

cbacdcb99da472fd99a0dc104f3cec1c93d8a97f
oscarolsson94 <oscar.olsson94@hotmail.com>
2021-07-07 10:45:29 +0200

    fetch dynamic products list from commercejs

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Navbar from './components/Navbar/Navbar';
 import Products from './components/Products/Products';
 
+// grab commerce instance which will handle entire backend
+import { commerce } from './lib/commerce';
+
 const App = () => {
+    const [products, setProducts] = useState([]);
+
+    const fetchProducts = async () => {
+        const { data } = await commerce.products.list();
+
+        setProducts(data);
+    }
+
     return (
         <div>
             <Navbar />

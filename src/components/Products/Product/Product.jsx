7b4218a59e5e410c25f7e89c913f1b1656dd7e4f
oscarolsson94 <oscar.olsson94@hotmail.com>
2021-07-07 09:10:55 +0200

    product content

--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import { Card, CardMedia, CardContent, CarcActions, Typography, IconButton } from '@material-ui/core';
+import { Card, CardMedia, CardContent, CardActions, Typography, IconButton } from '@material-ui/core';
 import{ AddShoppingCart } from '@material-ui/icons';
 
-const Product = () => {
+const Product = ({product}) => {
     return (
         <Card className={classes.root} >
             <CardMedia className={classes.media} image='' title={product.name} />
@@ -15,7 +15,13 @@ const Product = () => {
                         {product.price}
                     </Typography>
                 </div>
+                <Typography variant="h2" color="textSecondary">{product.description}</Typography>
             </CardContent>
+            <CardActions disableSpacing className={classes.cardActions}>  
+                <IconButton aria-label="Add to Cart" >
+                    <AddShoppingCart />
+                </IconButton>
+            </CardActions>
         </Card>
     )
 }

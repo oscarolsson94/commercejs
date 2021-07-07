cd83d1730c7c37a83aa5259a1941d46476b222dc
oscarolsson94 <oscar.olsson94@hotmail.com>
2021-07-07 12:08:11 +0200

    updating number of items in cart on change

--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -21,7 +21,7 @@ const Product = ({product, onAddToCart}) => {
                 <Typography dangerouslySetInnerHTML={{ __html: product.description }} variant="body2" color="textSecondary"/>
             </CardContent>
             <CardActions disableSpacing className={classes.cardActions}>  
-                <IconButton aria-label="Add to Cart" onClick={onAddToCart} >
+                <IconButton aria-label="Add to Cart" onClick={() => onAddToCart(product.id, 1)} >
                     <AddShoppingCart />
                 </IconButton>
             </CardActions>

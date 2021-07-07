fce2b8c0c500c934e72a3735afac05e22fffc1f2
oscarolsson94 <oscar.olsson94@hotmail.com>
2021-07-07 10:06:08 +0200

    adding items to navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,15 +3,25 @@ import { AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography } from '
 import { ShoppingCart } from '@material-ui/icons';
 import { classExpression } from '@babel/types';
 
+import logo from '../../assets/commerce.png';
+
 const Navbar = () => {
     return (
         <>
             <AppBar position="fixed" className={classExpression.appBar} color="inherit" >
                 <Toolbar>
                     <Typography variant="h6" className={classes.title} color="inherit" >
-                        <img src={} alt="Commerce.js" height="25px" className={classes.image} />
+                        <img src={logo} alt="Commerce.js" height="25px" className={classes.image} />
                         Company name
                     </Typography>
+                    <div className={classes.grow} />
+                    <div className={classes.button}>
+                        <IconButton  aria-label="Show cart items" color="inherit" >
+                            <Badge badgeContent={2} color="secondary" >
+                                <ShoppingCart />
+                            </Badge>
+                        </IconButton>
+                    </div>
                 </Toolbar>
             </AppBar>
         </>

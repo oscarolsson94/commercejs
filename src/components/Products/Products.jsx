65cf123e41e0bd72230f8bb8b81ef7bc33801365
oscarolsson94 <oscar.olsson94@hotmail.com>
2021-07-07 09:21:08 +0200

    formatting

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Grid} from '@material-ui/core';
+import { Grid } from '@material-ui/core';
 import Product from './Product/Product';
 
 const products = [
@@ -8,7 +8,7 @@ const products = [
 ];
 
 const Products = () => {
-    <main>
+    return <main>
         <Grid container justify="center" spacing={4} >
             {products.map((product) => (
                 <Grid item key={product.id} xs={12} sm={6} md= {4} lg={3}>

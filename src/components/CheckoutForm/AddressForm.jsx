2db3d29e87424ac1338c0f89da11b8910e69cb1e
oscarolsson94 <oscar.olsson94@hotmail.com>
2021-07-08 12:27:17 +0200

    starting to implement select options

--- src/components/CheckoutForm/AddressForm.jsx
+++ src/components/CheckoutForm/AddressForm.jsx
@@ -27,6 +27,31 @@ const AddressForm = () => {
             <FormInput required name="email" label="Email" />
             <FormInput required name="city" label="City" />
             <FormInput required name="zip" label="ZIP / Postal code" />
+            {/* DYNAMIC SELECT LIST, OPTIONS DEPENDING ON EACHOTHER */}
+            <Grid item xs={12} sm={6}>
+                <InputLabel>Shipping Country</InputLabel>
+                <Select value={} fullWidth onChange={}>
+                    <MenuItem key={} value={}>
+                        Select Me
+                    </MenuItem>
+                </Select>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+                <InputLabel>Shipping Subdivision</InputLabel>
+                <Select value={} fullWidth onChange={}>
+                    <MenuItem key={} value={}>
+                        Select Me
+                    </MenuItem>
+                </Select>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+                <InputLabel>Shipping Options</InputLabel>
+                <Select value={} fullWidth onChange={}>
+                    <MenuItem key={} value={}>
+                        Select Me
+                    </MenuItem>
+                </Select>
+            </Grid>
           </Grid>
         </form>
       </FormProvider>

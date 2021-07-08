8fd2f4bedbf3f39808c18243a6527c616d91e272
oscarolsson94 <oscar.olsson94@hotmail.com>
2021-07-08 09:33:30 +0200

    creating updateCartQty function

--- src/App.js
+++ src/App.js
@@ -1,55 +1,60 @@
-import React, { useState, useEffect } from 'react';
-import Cart from './components/Cart/Cart';
-import Navbar from './components/Navbar/Navbar';
-import Products from './components/Products/Products';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import Cart from "./components/Cart/Cart";
+import Navbar from "./components/Navbar/Navbar";
+import Products from "./components/Products/Products";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 // grab commerce instance which will handle entire backend
-import { commerce } from './lib/commerce';
+import { commerce } from "./lib/commerce";
 
 const App = () => {
-    const [products, setProducts] = useState([]);
-    const [cart, setCart] = useState([]);
+  const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState([]);
 
-    const fetchProducts = async () => {
-        const { data } = await commerce.products.list();
+  const fetchProducts = async () => {
+    const { data } = await commerce.products.list();
 
-        setProducts(data);
-    }
+    setProducts(data);
+  };
 
-    const fetchCart = async () => {
-        const cart = await commerce.cart.retrieve();
+  const fetchCart = async () => {
+    const cart = await commerce.cart.retrieve();
 
-        setCart(cart);
-    }
+    setCart(cart);
+  };
 
-    const handleAddToCart = async (productId, quantity) => {
-        const item = await commerce.cart.add(productId, quantity);
+  const handleAddToCart = async (productId, quantity) => {
+    const item = await commerce.cart.add(productId, quantity);
 
-        setCart(item.cart);
-    }
+    setCart(item.cart);
+  };
 
-    useEffect(() => {
-        fetchProducts();
-        fetchCart();
-    },[]);
+  const handleUpdateCartQty = async (productId, quantity) => {
+    const { cart } = await commerce.cart.update(productId, { quantity });
 
-    return (
-        <Router>
-        <div>
-            <Navbar totalItems={cart.total_items} />
-            <Switch>
-                <Route exact path="/" >
-                    <Products products={products} onAddToCart={handleAddToCart} />
-                </Route>
-                <Route exact path="/cart" >
-                    <Cart cart={cart} />
-                </Route>
-            </Switch>
-        </div>
-        </Router>
-    )
+    setCart(cart);
+  };
 
-}
+  useEffect(() => {
+    fetchProducts();
+    fetchCart();
+  }, []);
+
+  return (
+    <Router>
+      <div>
+        <Navbar totalItems={cart.total_items} />
+        <Switch>
+          <Route exact path="/">
+            <Products products={products} onAddToCart={handleAddToCart} />
+          </Route>
+          <Route exact path="/cart">
+            <Cart cart={cart} />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+};
 
 export default App;

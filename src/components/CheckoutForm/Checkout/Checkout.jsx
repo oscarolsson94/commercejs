f3d742ea64a399dfc255edd1f937b417d3b37f3c
oscarolsson94 <oscar.olsson94@hotmail.com>
2021-07-08 10:01:57 +0200

    importing components from materialUI

---
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import {
+  Paper,
+  Stepper,
+  Step,
+  StepLabel,
+  Typography,
+  CircularProgress,
+  Divider,
+  Button,
+} from "@material-ui/core";
+
+const Checkout = () => {
+  return (
+    <>
+      <div className={classes.toolbar}></div>
+      <main></main>
+    </>
+  );
+};
+
+export default Checkout;

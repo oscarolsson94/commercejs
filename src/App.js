2766254e53a05a2edcecd8960df114d03894a68c
oscarolsson94 <oscar.olsson94@hotmail.com>
2021-07-08 09:39:56 +0200

    passing handler functions to cart

--- src/App.js
+++ src/App.js
@@ -61,7 +61,12 @@ const App = () => {
             <Products products={products} onAddToCart={handleAddToCart} />
           </Route>
           <Route exact path="/cart">
-            <Cart cart={cart} />
+            <Cart
+              cart={cart}
+              handleUpdateCartQty={handleUpdateCartQty}
+              handleRemoveFromCart={handleRemoveFromCart}
+              handleEmptyCart={handleEmptyCart}
+            />
           </Route>
         </Switch>
       </div>

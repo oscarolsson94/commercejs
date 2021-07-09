319a53e1979a293f36fff8a27c6d0098af2fd190
oscarolsson94 <oscar.olsson94@hotmail.com>
2021-07-09 10:41:44 +0200

    removing not needed code

--- src/components/CheckoutForm/CustomTextField.jsx
+++ src/components/CheckoutForm/CustomTextField.jsx
@@ -2,15 +2,13 @@ import React from "react";
 import { TextField, Grid } from "@material-ui/core";
 import { useFormContext, Controller } from "react-hook-form";
 
-const FormInput = ({ name, label, required }) => {
+const FormInput = ({ name, label }) => {
   const { control } = useFormContext();
 
   return (
     <Grid item xs={12} sm={6}>
       <Controller
-        render={({ field }) => (
-          <TextField {...field} label={label} fullWidth required />
-        )}
+        render={({ field }) => <TextField {...field} label={label} fullWidth />}
         control={control}
         name={name}
       />

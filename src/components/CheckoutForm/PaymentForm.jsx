f11bbcc32f9a986cfc4c039e9d31e4aa07f3adcc
oscarolsson94 <oscar.olsson94@hotmail.com>
2021-07-13 09:31:25 +0200

    working on submitting form data

--- src/components/CheckoutForm/PaymentForm.jsx
+++ src/components/CheckoutForm/PaymentForm.jsx
@@ -11,6 +11,8 @@ import Review from "./Review";
 const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLIC_KEY);
 
 const PaymentForm = ({ checkoutToken, backStep }) => {
+  const handleSubmit = () => {};
+
   return (
     <>
       <Review checkoutToken={checkoutToken} />
@@ -21,7 +23,7 @@ const PaymentForm = ({ checkoutToken, backStep }) => {
       <Elements stripe={stripePromise}>
         <ElementsConsumer>
           {({ elements, stripe }) => (
-            <form>
+            <form onSubmit={(e) => handleSubmit(e, elements, stripe)}>
               <CardElement />
               <br /> <br />
               <div style={{ display: "flex", justifyContent: "space-between" }}>
